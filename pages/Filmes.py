d4b9e1b815460a852355d520b911079b6156cb92
castrochad <114541387+castrochad@users.noreply.github.com>
2023-12-15 20:33:16 -0300

    Add files via upload

--- pages/Filmes.py
+++ pages/Filmes.py
@@ -1,35 +1,22 @@
-import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# Carregar os dados
-file_path = 'IMDbMovies.csv'
-data = pd.read_csv(file_path)
-
-# Criar lista de títulos
-titles = data['Title'].unique().tolist()
-
-# Adicionar um seletor de filmes
-selected_movie = st.selectbox('Selecione um filme:', titles)
-
-# Exibir informações do filme selecionado
-selected_info = data[data['Title'] == selected_movie]
+# ...
 
 if not selected_info.empty:
-    st.subheader(f'Informações do filme: {selected_movie}')
-    st.write('Rating:', selected_info['Rating'].values[0])
-    st.write('Motion Picture Rating:', selected_info['Motion Picture Rating'].values[0])
-    st.write('Runtime:', selected_info['Runtime'].values[0])
-    st.write('Budget:', selected_info['Budget'].values[0])
-    st.write('Gross Worldwide:', selected_info['Gross worldwide'].values[0])
+    # ... (exibir informações do filme)
 
-    # Gráfico com o valor de Gross worldwide, Gross in US & Canada e Opening Weekend Gross
-    gross_values = selected_info[['Gross worldwide', 'Gross in US & Canada', 'Opening Weekend Gross in US & Canada']].values[0]
-    labels = ['Gross worldwide', 'Gross in US & Canada', 'Opening Weekend Gross']
-    
-    fig, ax = plt.subplots()
-    ax.pie(gross_values, labels=labels, autopct='%1.1f%%', startangle=90)
-    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    st.pyplot(fig)
+    # Verificar se os dados de Gross Worldwide, Gross in US & Canada e Opening Weekend Gross estão disponíveis
+    if 'Gross worldwide' in selected_info.columns and 'Gross in US & Canada' in selected_info.columns and 'Opening Weekend Gross in US & Canada' in selected_info.columns:
+        gross_values = selected_info[['Gross worldwide', 'Gross in US & Canada', 'Opening Weekend Gross in US & Canada']].values[0]
+        labels = ['Gross worldwide', 'Gross in US & Canada', 'Opening Weekend Gross']
+
+        # Verificar se há valores nulos
+        if not pd.isnull(gross_values).any():
+            fig, ax = plt.subplots()
+            ax.pie(gross_values, labels=labels, autopct='%1.1f%%', startangle=90)
+            ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+            st.pyplot(fig)
+        else:
+            st.write('Há valores ausentes para criar o gráfico de pizza.')
+    else:
+        st.write('As colunas necessárias para criar o gráfico não estão presentes no conjunto de dados.')
 else:
     st.write('Nenhuma informação disponível para o filme selecionado.')
