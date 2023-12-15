b69eac363be01ecaf486461278d938f0c4e7b1ea
castrochad <114541387+castrochad@users.noreply.github.com>
2023-12-15 18:20:37 -0300

    Add files via upload

--- pages/Filmes.py
+++ pages/Filmes.py
@@ -9,24 +9,24 @@ def load_data(file_path):
 file_path = 'IMDbMovies.csv'
 data = load_data(file_path)
 
-# Filtrar por Release Year
+# Filtro por Release Year
 selected_year = st.sidebar.selectbox('Selecione o ano de lançamento:', data['Release Year'].unique())
 filtered_by_year = data[data['Release Year'] == selected_year]
 
 # Filtrar por Runtime
-runtime_1h = filtered_by_year[filtered_by_year['Runtime'] == '1h']
-runtime_2h = filtered_by_year[filtered_by_year['Runtime'] == '2h']
-runtime_3h = filtered_by_year[filtered_by_year['Runtime'] == '3h']
+runtime_1h = filtered_by_year[filtered_by_year['Runtime'] < '2h']
+runtime_2h = filtered_by_year[(filtered_by_year['Runtime'] >= '2h') & (filtered_by_year['Runtime'] < '3h')]
+runtime_3h = filtered_by_year[filtered_by_year['Runtime'] >= '3h']
 
 # Exibir os resultados
 st.write(f"Filmes lançados no ano de {selected_year}:")
 st.write(filtered_by_year)
 
-st.write("Filmes com duração de 1h:")
+st.write("Filmes com menos de 2 horas de duração:")
 st.write(runtime_1h)
 
-st.write("Filmes com duração de 2h:")
+st.write("Filmes com duração entre 2 e 3 horas:")
 st.write(runtime_2h)
 
-st.write("Filmes com duração de 3h:")
+st.write("Filmes com mais de 3 horas de duração:")
 st.write(runtime_3h)
