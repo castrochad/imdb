2fe9bc09c06ee4eebbd8426df947657d17a0fc9f
castrochad <114541387+castrochad@users.noreply.github.com>
2023-12-15 20:44:06 -0300

    Add files via upload

--- pages/Filmes.py
+++ pages/Filmes.py
@@ -1,9 +1,29 @@
-# ...
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Carregar os dados
+file_path = 'IMDbMovies.csv'
+data = pd.read_csv(file_path)
+
+# Criar lista de títulos
+titles = data['Title'].unique().tolist()
+
+# Adicionar um seletor de filmes
+selected_movie = st.selectbox('Selecione um filme:', titles)
+
+# Exibir informações do filme selecionado
+selected_info = data[data['Title'] == selected_movie]
 
 if not selected_info.empty:
-    # ... (exibir informações do filme)
+    st.subheader(f'Informações do filme: {selected_movie}')
+    st.write('Rating:', selected_info['Rating'].values[0])
+    st.write('Motion Picture Rating:', selected_info['Motion Picture Rating'].values[0])
+    st.write('Runtime:', selected_info['Runtime'].values[0])
+    st.write('Budget:', selected_info['Budget'].values[0])
+    st.write('Gross Worldwide:', selected_info['Gross worldwide'].values[0])
 
-    # Verificar se os dados de Gross Worldwide, Gross in US & Canada e Opening Weekend Gross estão disponíveis
+    # Verificar se as colunas necessárias para o gráfico estão presentes
     if 'Gross worldwide' in selected_info.columns and 'Gross in US & Canada' in selected_info.columns and 'Opening Weekend Gross in US & Canada' in selected_info.columns:
         gross_values = selected_info[['Gross worldwide', 'Gross in US & Canada', 'Opening Weekend Gross in US & Canada']].values[0]
         labels = ['Gross worldwide', 'Gross in US & Canada', 'Opening Weekend Gross']
