25c3c7f649569bcad256b1317c46a3e5e6d2f3e7
castrochad <114541387+castrochad@users.noreply.github.com>
2023-12-15 19:25:21 -0300

    Add files via upload

--- pages/Budget.py
+++ pages/Budget.py
@@ -6,43 +6,30 @@ import matplotlib.pyplot as plt
 file_path = 'IMDbMovies.csv'
 data = pd.read_csv(file_path)
 
-# Remover linhas onde 'Budget' ou 'Gross worldwide' são nulos para evitar erros
-data = data.dropna(subset=['Budget', 'Gross worldwide'])
-
-# Gráfico dos maiores Budgets
-st.subheader("Top 10 Maiores Budgets")
+# Gráfico dos 10 maiores Budgets
+st.subheader('Top 10 Maiores Orçamentos')
 top_budget = data.nlargest(10, 'Budget')
 plt.figure(figsize=(10, 6))
 plt.barh(top_budget['Title'], top_budget['Budget'])
-plt.xlabel('Budget')
-plt.title('Top 10 Maiores Budgets')
+plt.xlabel('Orçamento')
+plt.title('Top 10 Maiores Orçamentos')
 st.pyplot()
 
-# Gráfico dos maiores Gross Worldwide
-st.subheader("Top 10 Maiores Gross Worldwide")
+# Gráfico dos 10 maiores Gross Worldwide
+st.subheader('Top 10 Maiores Ganhas Globais')
 top_gross = data.nlargest(10, 'Gross worldwide')
 plt.figure(figsize=(10, 6))
 plt.barh(top_gross['Title'], top_gross['Gross worldwide'])
 plt.xlabel('Gross Worldwide')
-plt.title('Top 10 Maiores Gross Worldwide')
+plt.title('Top 10 Maiores Ganhas Globais')
 st.pyplot()
 
-# Gráfico de orçamento ao passar dos anos
-st.subheader("Orçamento ao Passar dos Anos")
-budget_over_years = data.groupby('Release Year')['Budget'].sum().reset_index()
-budget_over_years['Release Year'] = budget_over_years['Release Year'].astype(str)
+# Gráfico de Budget geral por Release Year
+st.subheader('Orçamento Geral por Ano de Lançamento')
+budget_by_year = data.groupby('Release Year')['Budget'].sum()
 plt.figure(figsize=(10, 6))
-plt.plot(budget_over_years['Release Year'], budget_over_years['Budget'], marker='o')
+budget_by_year.plot(kind='bar')
 plt.xlabel('Ano de Lançamento')
 plt.ylabel('Orçamento Total')
-plt.title('Orçamento ao Passar dos Anos')
-st.pyplot()
-
-# Gráfico dos maiores Motion Picture Rating
-st.subheader("Top 10 Maiores Motion Picture Rating")
-top_rating = data.nlargest(10, 'Motion Picture Rating')
-plt.figure(figsize=(10, 6))
-plt.barh(top_rating['Title'], top_rating['Motion Picture Rating'])
-plt.xlabel('Motion Picture Rating')
-plt.title('Top 10 Maiores Motion Picture Rating')
+plt.title('Orçamento Geral por Ano de Lançamento')
 st.pyplot()
