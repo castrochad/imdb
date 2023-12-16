a7bb39fdbecd4659c1d3be8c6e4516eb46d9cc77
castrochad <114541387+castrochad@users.noreply.github.com>
2023-12-15 21:16:16 -0300

    Add files via upload

--- pages/Filmes.py
+++ pages/Filmes.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import re
 
 # Carregar os dados
 file_path = 'IMDbMovies.csv'
@@ -26,8 +27,30 @@ if not selected_info.empty:
     gross_us_canada = selected_info['Gross in US & Canada'].values[0]
     opening_weekend_gross = selected_info['Opening Weekend Gross in US & Canada'].values[0]
     
-    st.write(f'Gross worldwide: {gross_worldwide}')
-    st.write(f'Gross in US & Canada: {gross_us_canada}')
-    st.write(f'Opening Weekend Gross in US & Canada: {opening_weekend_gross}')
+    # Função para remover caracteres não numéricos e converter para float
+    def clean_and_convert(value):
+        cleaned_value = re.sub(r'[^\d.]', '', value)  # Remove tudo exceto dígitos e pontos
+        try:
+            return float(cleaned_value)
+        except ValueError:
+            return None  # Em caso de erro na conversão
+    
+    gross_worldwide = clean_and_convert(gross_worldwide)
+    gross_us_canada = clean_and_convert(gross_us_canada)
+    opening_weekend_gross = clean_and_convert(opening_weekend_gross)
+    
+    st.write(f'Gross worldwide (cleaned): {gross_worldwide}')
+    st.write(f'Gross in US & Canada (cleaned): {gross_us_canada}')
+    st.write(f'Opening Weekend Gross in US & Canada (cleaned): {opening_weekend_gross}')
+    
+    # Calcular percentuais
+    if gross_worldwide:
+        percentage_gross_us_canada = (gross_us_canada / gross_worldwide) * 100
+        percentage_opening_weekend = (opening_weekend_gross / gross_worldwide) * 100
+        
+        st.write(f'Percentage of Gross in US & Canada: {percentage_gross_us_canada:.2f}%')
+        st.write(f'Percentage of Opening Weekend Gross: {percentage_opening_weekend:.2f}%')
+    else:
+        st.write('Não é possível calcular os percentuais.')
 else:
     st.write('Nenhuma informação disponível para o filme selecionado.')
