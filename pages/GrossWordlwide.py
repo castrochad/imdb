10ae4cb9a51bd45b5c1992a8d35aad2b542cc54e
castrochad <114541387+castrochad@users.noreply.github.com>
2023-12-15 22:11:03 -0300

    Add files via upload

--- pages/GrossWordlwide.py
+++ pages/GrossWordlwide.py
@@ -1,20 +1,19 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
 # Carregar os dados
 file_path = 'IMDbMovies.csv'
 data = pd.read_csv(file_path)
 
-# Remover linhas com valores ausentes em 'Gross worldwide' e 'Budget'
-data = data.dropna(subset=['Gross worldwide', 'Budget'])
-
 # Calcular a porcentagem do orçamento em relação ao 'Gross worldwide'
-data['pct_do_orcamento'] = np.where(data['Budget'] != 0, data['Gross worldwide'] / data['Budget'], 0)
+data['pct_do_orcamento'] = (data['Budget'] / data['Gross worldwide']) * 100
 
 # Calcular as porcentagens para 'Gross in US & Canada' e 'Opening Weekend Gross in US & Canada'
-data['pct_gross_us_canada'] = np.where(data['Gross worldwide'] != 0, data['Gross in US & Canada'] / data['Gross worldwide'], 0)
-data['pct_opening_weekend'] = np.where(data['Gross worldwide'] != 0, data['Opening Weekend Gross in US & Canada'] / data['Gross worldwide'], 0)
+data['pct_gross_us_canada'] = (data['Gross in US & Canada'] / data['Gross worldwide']) * 100
+data['pct_opening_weekend'] = (data['Opening Weekend Gross in US & Canada'] / data['Gross worldwide']) * 100
+
+# Criar coluna de relação entre Rating e Number of Ratings
+data['rating_rel_number_of_ratings'] = data['Rating'] / data['Number of Ratings']
 
 # Exibir os dados processados
-st.write(data[['Gross worldwide', 'Budget', 'pct_do_orcamento', 'Gross in US & Canada', 'Opening Weekend Gross in US & Canada', 'pct_gross_us_canada', 'pct_opening_weekend']])
+st.write(data[['Budget', 'Gross worldwide', 'pct_do_orcamento', 'Gross in US & Canada', 'Opening Weekend Gross in US & Canada', 'pct_gross_us_canada', 'pct_opening_weekend', 'Rating', 'Number of Ratings', 'rating_rel_number_of_ratings']])
