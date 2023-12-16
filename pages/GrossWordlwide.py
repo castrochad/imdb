dab5b102971b63cd26f1f585d15cc5a730afd1f1
castrochad <114541387+castrochad@users.noreply.github.com>
2023-12-15 22:19:00 -0300

    Add files via upload

--- pages/GrossWordlwide.py
+++ pages/GrossWordlwide.py
@@ -5,25 +5,28 @@ import pandas as pd
 file_path = 'IMDbMovies.csv'
 data = pd.read_csv(file_path)
 
-# Verificar se há valores nulos ou zeros em 'Budget' e 'Gross worldwide'
-null_budget = data['Budget'].isnull().sum()
-null_gross_worldwide = data['Gross worldwide'].isnull().sum()
-zero_budget = (data['Budget'] == 0).sum()
-zero_gross_worldwide = (data['Gross worldwide'] == 0).sum()
+# Verificar e tratar valores nulos em 'Budget' e 'Gross worldwide'
+null_budget = data['Budget'].isnull()
+null_gross_worldwide = data['Gross worldwide'].isnull()
+zero_budget = data['Budget'] == 0
+zero_gross_worldwide = data['Gross worldwide'] == 0
 
-# Tratar os valores nulos ou zeros
-if null_budget > 0 or null_gross_worldwide > 0 or zero_budget > 0 or zero_gross_worldwide > 0:
-    st.write("Existem valores nulos ou zeros em 'Budget' ou 'Gross worldwide'.")
-else:
-    # Calcular a porcentagem do orçamento em relação ao 'Gross worldwide'
-    data['pct_do_orcamento'] = (data['Budget'] / data['Gross worldwide']) * 100
+# Preencher valores nulos com a média e substituir zeros por média em 'Budget' e 'Gross worldwide'
+mean_budget = data.loc[~(null_budget | zero_budget), 'Budget'].mean()
+mean_gross = data.loc[~(null_gross_worldwide | zero_gross_worldwide), 'Gross worldwide'].mean()
 
-    # Calcular as porcentagens para 'Gross in US & Canada' e 'Opening Weekend Gross in US & Canada'
-    data['pct_gross_us_canada'] = (data['Gross in US & Canada'] / data['Gross worldwide']) * 100
-    data['pct_opening_weekend'] = (data['Opening Weekend Gross in US & Canada'] / data['Gross worldwide']) * 100
+data.loc[null_budget | zero_budget, 'Budget'] = mean_budget
+data.loc[null_gross_worldwide | zero_gross_worldwide, 'Gross worldwide'] = mean_gross
 
-    # Criar coluna de relação entre Rating e Number of Ratings
-    data['rating_rel_number_of_ratings'] = data['Rating'] / data['Number of Ratings']
+# Calcular a porcentagem do orçamento em relação ao 'Gross worldwide'
+data['pct_do_orcamento'] = (data['Budget'] / data['Gross worldwide']) * 100
 
-    # Exibir os dados processados
-    st.write(data[['Budget', 'Gross worldwide', 'pct_do_orcamento', 'Gross in US & Canada', 'Opening Weekend Gross in US & Canada', 'pct_gross_us_canada', 'pct_opening_weekend', 'Rating', 'Number of Ratings', 'rating_rel_number_of_ratings']])
+# Calcular as porcentagens para 'Gross in US & Canada' e 'Opening Weekend Gross in US & Canada'
+data['pct_gross_us_canada'] = (data['Gross in US & Canada'] / data['Gross worldwide']) * 100
+data['pct_opening_weekend'] = (data['Opening Weekend Gross in US & Canada'] / data['Gross worldwide']) * 100
+
+# Criar coluna de relação entre Rating e Number of Ratings
+data['rating_rel_number_of_ratings'] = data['Rating'] / data['Number of Ratings']
+
+# Exibir os dados processados
+st.write(data[['Budget', 'Gross worldwide', 'pct_do_orcamento', 'Gross in US & Canada', 'Opening Weekend Gross in US & Canada', 'pct_gross_us_canada', 'pct_opening_weekend', 'Rating', 'Number of Ratings', 'rating_rel_number_of_ratings']])
