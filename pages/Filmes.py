dbdad52f2f40a0aa4530d604c0067ffc5495e525
castrochad <114541387+castrochad@users.noreply.github.com>
2023-12-15 21:04:59 -0300

    Add files via upload

--- pages/Filmes.py
+++ pages/Filmes.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
 # Carregar os dados
 file_path = 'IMDbMovies.csv'
@@ -22,24 +21,16 @@ if not selected_info.empty:
     st.write('Runtime:', selected_info['Runtime'].values[0])
     st.write('Budget:', selected_info['Budget'].values[0])
     st.write('Gross Worldwide:', selected_info['Gross worldwide'].values[0])
-
+    
+    gross_worldwide = float(selected_info['Gross worldwide'].values[0])
+    gross_us_canada = float(selected_info['Gross in US & Canada'].values[0])
+    opening_weekend_gross = float(selected_info['Opening Weekend Gross in US & Canada'].values[0])
+    
     # Cálculo dos percentuais
-    gross_values = [
-        float(selected_info['Gross in US & Canada'].values[0]),
-        float(selected_info['Opening Weekend Gross in US & Canada'].values[0]),
-        float(selected_info['Gross worldwide'].values[0])
-    ]
+    percent_gross_us_canada = (gross_us_canada / gross_worldwide) * 100
+    percent_opening_weekend = (opening_weekend_gross / gross_worldwide) * 100
     
-    st.write('Valores brutos:', gross_values)
-    
-    labels = ['Gross in US & Canada', 'Opening Weekend Gross in US & Canada', 'Gross worldwide']
-
-    if not pd.isnull(gross_values).any():
-        fig, ax = plt.subplots()
-        ax.pie(gross_values, labels=labels, autopct='%1.1f%%', startangle=90, textprops={'fontsize': 12})
-        ax.axis('equal')
-        st.pyplot(fig)
-    else:
-        st.write('Há valores ausentes para criar o gráfico de pizza.')
+    st.write(f'Percentual de Gross in US & Canada em relação ao Gross Worldwide: {percent_gross_us_canada:.2f}%')
+    st.write(f'Percentual de Opening Weekend Gross em relação ao Gross Worldwide: {percent_opening_weekend:.2f}%')
 else:
     st.write('Nenhuma informação disponível para o filme selecionado.')
