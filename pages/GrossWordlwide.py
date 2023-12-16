ca85bea614f6b4f586958fde64baef1128f2178e
castrochad <114541387+castrochad@users.noreply.github.com>
2023-12-15 21:52:58 -0300

    Add files via upload

--- pages/GrossWordlwide.py
+++ pages/GrossWordlwide.py
@@ -9,11 +9,11 @@ data = pd.read_csv(file_path)
 data = data.dropna(subset=['Gross worldwide', 'Budget'])
 
 # Verificar se os valores são números antes de realizar a operação
-data['pct_do_orcamento'] = data.apply(lambda row: row['Gross worldwide'] / row['Budget'] if pd.to_numeric(row['Budget'], errors='coerce') != 0 else 0, axis=1)
+data['pct_do_orcamento'] = data['Gross worldwide'] / data['Budget'].replace(0, pd.NA)
 
 # Calcular percentuais para 'Gross in US & Canada' e 'Opening Weekend Gross in US & Canada'
-data['pct_gross_us_canada'] = data.apply(lambda row: row['Gross in US & Canada'] / row['Gross worldwide'] if row['Gross worldwide'] != 0 else 0, axis=1)
-data['pct_opening_weekend'] = data.apply(lambda row: row['Opening Weekend Gross in US & Canada'] / row['Gross worldwide'] if row['Gross worldwide'] != 0 else 0, axis=1)
+data['pct_gross_us_canada'] = data['Gross in US & Canada'] / data['Gross worldwide'].replace(0, pd.NA)
+data['pct_opening_weekend'] = data['Opening Weekend Gross in US & Canada'] / data['Gross worldwide'].replace(0, pd.NA)
 
 # Exibir os dados processados
 st.write(data[['Gross worldwide', 'Budget', 'pct_do_orcamento', 'Gross in US & Canada', 'Opening Weekend Gross in US & Canada', 'pct_gross_us_canada', 'pct_opening_weekend']])
