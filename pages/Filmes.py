c902d65fa5891978a362ea31d484cc3e543545d4
castrochad <114541387+castrochad@users.noreply.github.com>
2023-12-15 21:07:48 -0300

    Add files via upload

--- pages/Filmes.py
+++ pages/Filmes.py
@@ -22,15 +22,12 @@ if not selected_info.empty:
     st.write('Budget:', selected_info['Budget'].values[0])
     st.write('Gross Worldwide:', selected_info['Gross worldwide'].values[0])
     
-    gross_worldwide = float(selected_info['Gross worldwide'].values[0])
-    gross_us_canada = float(selected_info['Gross in US & Canada'].values[0])
-    opening_weekend_gross = float(selected_info['Opening Weekend Gross in US & Canada'].values[0])
+    gross_worldwide = selected_info['Gross worldwide'].values[0]
+    gross_us_canada = selected_info['Gross in US & Canada'].values[0]
+    opening_weekend_gross = selected_info['Opening Weekend Gross in US & Canada'].values[0]
     
-    # Cálculo dos percentuais
-    percent_gross_us_canada = (gross_us_canada / gross_worldwide) * 100
-    percent_opening_weekend = (opening_weekend_gross / gross_worldwide) * 100
-    
-    st.write(f'Percentual de Gross in US & Canada em relação ao Gross Worldwide: {percent_gross_us_canada:.2f}%')
-    st.write(f'Percentual de Opening Weekend Gross em relação ao Gross Worldwide: {percent_opening_weekend:.2f}%')
+    st.write(f'Gross worldwide: {gross_worldwide}')
+    st.write(f'Gross in US & Canada: {gross_us_canada}')
+    st.write(f'Opening Weekend Gross in US & Canada: {opening_weekend_gross}')
 else:
     st.write('Nenhuma informação disponível para o filme selecionado.')
