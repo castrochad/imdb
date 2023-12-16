e4617322b150e10bc6d091596c607948399190a7
castrochad <114541387+castrochad@users.noreply.github.com>
2023-12-15 23:25:56 -0300

    Add files via upload

--- pages/GrossWordlwide.py
+++ pages/GrossWordlwide.py
@@ -1,46 +1,46 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Carregar os dados
 file_path = 'IMDbMovies.csv'
 data = pd.read_csv(file_path)
 
-# Adicionar um seletor de filmes
-titles = data['Title'].unique().tolist()
-selected_movie = st.selectbox('Selecione um filme:', titles)
+# Transformar as colunas numéricas necessárias para float
+numeric_columns = ['Budget', 'Gross in US & Canada', 'Gross worldwide', 'Opening Weekend Gross in US & Canada']
+data[numeric_columns] = data[numeric_columns].apply(pd.to_numeric, errors='coerce')
 
-# Filtrar os dados com base no filme selecionado
-selected_info = data[data['Title'] == selected_movie]
+# Agrupar os dados por ano e calcular as estatísticas
+grouped_data = data.groupby('Release Year').agg({
+    'Budget': 'sum',
+    'Gross worldwide': 'sum',
+    'Gross in US & Canada': 'sum',
+    'Opening Weekend Gross in US & Canada': 'sum'
+}).reset_index()
 
+# Calcular os percentuais
+grouped_data['Pct_Budget_to_Worldwide'] = (grouped_data['Budget'] / grouped_data['Gross worldwide']) * 100
+grouped_data['Pct_USCanada_to_Worldwide'] = (grouped_data['Gross in US & Canada'] / grouped_data['Gross worldwide']) * 100
+grouped_data['Pct_OpeningWeekend_to_Worldwide'] = (grouped_data['Opening Weekend Gross in US & Canada'] / grouped_data['Gross worldwide']) * 100
+
+# Exibir o seletor de ano
+selected_year = st.selectbox('Selecione um ano:', grouped_data['Release Year'])
+
+# Filtrar os dados para o ano selecionado
+selected_info = grouped_data[grouped_data['Release Year'] == selected_year]
+
+# Exibir as informações se houver dados para o ano selecionado
 if not selected_info.empty:
-    # Obter informações específicas do filme selecionado
-    budget = pd.to_numeric(selected_info['Budget'], errors='coerce').values[0]
-    gross_worldwide = pd.to_numeric(selected_info['Gross worldwide'], errors='coerce').values[0]
-    gross_us_canada = pd.to_numeric(selected_info['Gross in US & Canada'], errors='coerce').values[0]
-    opening_weekend_us_canada = pd.to_numeric(selected_info['Opening Weekend Gross in US & Canada'], errors='coerce').values[0]
-    rating = selected_info['Rating'].values[0]
-    num_ratings = selected_info['Number of Ratings'].values[0]
+    st.write(f'Informações para o ano {selected_year}:')
+    st.write(selected_info)
 
-    # Verificar se os valores são válidos antes de calcular as porcentagens
-    if pd.notnull(budget) and pd.notnull(gross_worldwide) and gross_worldwide != 0:
-        pct_do_orcamento = budget / gross_worldwide * 100
-        pct_gross_us_canada = gross_us_canada / gross_worldwide * 100
-        pct_opening_weekend = opening_weekend_us_canada / gross_worldwide * 100
-
-        st.write(f'Informações do filme: {selected_movie}')
-        st.write(f'Budget: {budget}')
-        st.write(f'Gross worldwide: {gross_worldwide}')
-        st.write(f'Pct_do_orcamento: {pct_do_orcamento:.2f}%')
-        st.write(f'Gross in US & Canada: {gross_us_canada}')
-        st.write(f'Opening Weekend Gross in US & Canada: {opening_weekend_us_canada}')
-        st.write(f'Pct_gross_us_canada: {pct_gross_us_canada:.2f}%')
-        st.write(f'Pct_opening_weekend: {pct_opening_weekend:.2f}%')
-        st.write(f'Rating: {rating}')
-        st.write(f'Number of Ratings: {num_ratings}')
-        st.write(f'Rating_rel_number_of_ratings: {rating / num_ratings:.2f}')
-
-    else:
-        st.write('Nenhuma informação disponível para o filme selecionado ou valores inválidos.')
+    # Criar um gráfico para exibir os percentuais
+    plt.figure(figsize=(10, 6))
+    plt.bar(selected_info.columns[4:], selected_info.values[0][4:], color=['blue', 'orange', 'green'])
+    plt.xlabel('Tipos de percentuais')
+    plt.ylabel('Valores em %')
+    plt.title(f'Percentuais para o ano {selected_year}')
+    st.pyplot(plt)
 
 else:
-    st.write('Nenhuma informação disponível para o filme selecionado.')
+    st.write('Nenhuma informação disponível para o ano selecionado.')
