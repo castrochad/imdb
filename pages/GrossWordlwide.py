a140984a541ab04e512bfe12dac685a1213113d0
castrochad <114541387+castrochad@users.noreply.github.com>
2023-12-15 21:56:45 -0300

    Add files via upload

--- pages/GrossWordlwide.py
+++ pages/GrossWordlwide.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 # Carregar os dados
 file_path = 'IMDbMovies.csv'
@@ -8,12 +9,12 @@ data = pd.read_csv(file_path)
 # Remover linhas com valores ausentes em 'Gross worldwide' e 'Budget'
 data = data.dropna(subset=['Gross worldwide', 'Budget'])
 
-# Verificar se os valores são números antes de realizar a operação
-data['pct_do_orcamento'] = data['Gross worldwide'] / data['Budget'].replace(0, pd.NA)
+# Calcular a porcentagem do orçamento em relação ao 'Gross worldwide'
+data['pct_do_orcamento'] = np.where(data['Budget'] != 0, data['Gross worldwide'] / data['Budget'], 0)
 
-# Calcular percentuais para 'Gross in US & Canada' e 'Opening Weekend Gross in US & Canada'
-data['pct_gross_us_canada'] = data['Gross in US & Canada'] / data['Gross worldwide'].replace(0, pd.NA)
-data['pct_opening_weekend'] = data['Opening Weekend Gross in US & Canada'] / data['Gross worldwide'].replace(0, pd.NA)
+# Calcular as porcentagens para 'Gross in US & Canada' e 'Opening Weekend Gross in US & Canada'
+data['pct_gross_us_canada'] = np.where(data['Gross worldwide'] != 0, data['Gross in US & Canada'] / data['Gross worldwide'], 0)
+data['pct_opening_weekend'] = np.where(data['Gross worldwide'] != 0, data['Opening Weekend Gross in US & Canada'] / data['Gross worldwide'], 0)
 
 # Exibir os dados processados
 st.write(data[['Gross worldwide', 'Budget', 'pct_do_orcamento', 'Gross in US & Canada', 'Opening Weekend Gross in US & Canada', 'pct_gross_us_canada', 'pct_opening_weekend']])
