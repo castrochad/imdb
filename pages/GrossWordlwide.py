3213afbab683200372de032dba2d168a5b92ee7c
castrochad <114541387+castrochad@users.noreply.github.com>
2023-12-15 22:43:05 -0300

    Add files via upload

--- pages/GrossWordlwide.py
+++ pages/GrossWordlwide.py
@@ -9,6 +9,18 @@ data = pd.read_csv(file_path)
 data['Budget'] = pd.to_numeric(data['Budget'], errors='coerce')
 data['Gross worldwide'] = pd.to_numeric(data['Gross worldwide'], errors='coerce')
 
+# Remover linhas com valores nulos em 'Budget' e 'Gross worldwide'
+data = data.dropna(subset=['Budget', 'Gross worldwide'])
+
+# Verificar se existem valores iguais a zero em 'Budget' e 'Gross worldwide'
+zero_budget = data['Budget'] == 0
+zero_gross = data['Gross worldwide'] == 0
+
+# Exibir contagem de valores nulos, zeros e linhas vazias
+st.write(f'Valores nulos em Budget: {data["Budget"].isnull().sum()}')
+st.write(f'Valores nulos em Gross worldwide: {data["Gross worldwide"].isnull().sum()}')
+st.write(f'Valores zero em Budget: {zero_budget.sum()}')
+st.write(f'Valores zero em Gross worldwide: {zero_gross.sum()}')
 
 # Calcular a porcentagem do orçamento em relação ao 'Gross worldwide'
 data['pct_do_orcamento'] = (data['Budget'] / data['Gross worldwide']) * 100
